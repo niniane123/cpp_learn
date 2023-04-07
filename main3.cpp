8266c0f2c66104f13e72aad3549eaed81433fe5e
Niniane <2307784147@qq.com>
2023-04-07 21:01:55 +0800

    commmit:提交命名空间

---
+++ main3.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World main3!" << std::endl;
+    return 0;
+}
---
+++ variable/nameing_rules.cpp
@@ -0,0 +1,12 @@
+//变量名的第一个字符不能是数字
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "hello world";
+    return 1;
+}
+
+
+
