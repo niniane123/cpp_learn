892d7db743426dd5dba31fda0b137124370e52fc
Niniane <2307784147@qq.com>
2023-04-09 15:37:11 +0800

    commit:浮点数类型内存占用大小

---
+++ function/floatsize.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "float类型：" << sizeof(float) << endl;
+    cout << "double类型：" << sizeof(double) << endl;
+    cout << "long double类型：" << sizeof(long double) << endl;
+}
