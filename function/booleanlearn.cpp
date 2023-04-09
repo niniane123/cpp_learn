54e6a5a9365c071e39982a96ed88225a8d212e78
Niniane <2307784147@qq.com>
2023-04-09 20:12:42 +0800

    提交：布尔了类型学习

---
+++ function/booleanlearn.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Niniane on 2023/4/9.
+//
+using namespace std;
+
+#include <iostream>
+
+int main() {
+    bool a = 100, b = false, c = "s", d = "fdjkfa";
+    //1
+    cout << a << endl;
+    cout << c << endl;
+
+    cout << d << endl;
+    // 布尔类型的大小
+    //sizeof(bool)1
+    cout << "sizeof(bool)" << sizeof(b) << endl;
+    cout << "请输入a:" << endl;
+    cin >> a;
+    cout << "a is " << a << endl;
+}
