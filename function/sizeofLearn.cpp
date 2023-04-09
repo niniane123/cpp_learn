fb4e85b65e4fbed930262d5b1e484565e4e6bf5e
Niniane <2307784147@qq.com>
2023-04-09 10:44:30 +0800

    commit:变量内存大小

---
+++ function/sizeofLearn.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Niniane on 2023/4/8.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "bool类型：" << sizeof(bool) << endl;
+    cout << "char类型：" << sizeof(char) << endl;
+    cout << "unsigned char类型：" << sizeof(unsigned char) << endl;
+    cout << "short类型" << sizeof(short) << endl;
+    cout << "unsigned short:" << sizeof(unsigned short) << endl;
+    cout << "int 类型：" << sizeof(int) << endl;
+    cout << "unsigned int 类型：" << sizeof(unsigned int) << endl;
+    cout << "long类型:" << sizeof(long) << endl;
+    cout << "unsigned long类型:" << sizeof(unsigned long) << endl;
+    cout << "long long类型" << sizeof(long long) << endl;
+    cout << "float类型" << sizeof(float) << endl;
+    cout << "doulble类型" << sizeof(double) << endl;
+    cout << "long double类型：" << sizeof(long double) << endl;
+}
