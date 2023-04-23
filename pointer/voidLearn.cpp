4d867598aa14ae4c9ca358370c4d610b976d9216
Niniane <2307784147@qq.com>
2023-04-23 23:25:29 +0800

    提交：void*注释

--- pointer/voidLearn.cpp
+++ pointer/voidLearn.cpp
@@ -2,7 +2,6 @@
 // Created by Niniane on 2023/4/19.
 //
 #include <iostream>
-#include <stdio.h>
 
 //宏常量的命名一般用大写  常量指针是一个指针，只不过它指向的对象是一个不可以通过解引用修改的常量
 using namespace std;
@@ -15,7 +14,7 @@ int main() {
 //    void 不能直接用来申明变量 但是void*可以
     int a = 100;
     void *pa = &a;
-    // 因为pa始终是一个指针嘛，所以我们可以直接得到pa保存的内存地址
+    // 因为pa始终是一个指针嘛，所以我们可以直接得到pa保存的内存地址，要想取值必须得解引用才能取值；
     cout << "指针pa所指向的内存地址为：" << pa << endl;
 
     //    不能直接对void*类型的指针解引用，必须做强制类型转换之后解引用
