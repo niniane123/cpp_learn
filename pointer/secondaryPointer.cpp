f80a61e9baba565ac35704bd2b5d5cd4a99d7713
Niniane <2307784147@qq.com>
2023-04-24 11:57:43 +0800

    提交：指针解引用即为指针所指向对象的值

---
+++ pointer/secondaryPointer.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Niniane on 2023/4/24.
+//
+#include <iostream>
+
+//宏常量的命名一般用大写
+
+using namespace std;
+
+int main() {
+    int i = 10;
+    cout << "i的地址为：" << &i << " i的值为" << i << endl;
+    //pi的值为所指向的内存地址
+    //通过对指针pi解引用可以获取其指向内存地址的变量值
+    //指针本身还占据了内存地址，所以有了指向指针的指针
+
+    int *pi = &i;
+    cout << "pi的值为" << pi << " 指针pi的解引用为" << *pi << " pi的地址为" << &pi << endl;
+
+    int **ppi = &pi;
+    //ppi的值应该是pi的内存地址
+    //解引用，那就是pi的值，即pi指针存放的i的地址变量 pi的值为0x3f523ff67c  指针ppi的解引用为0x3f523ff67c
+    cout << "ppi的值为" << ppi << " 指针ppi的解引用为" << *ppi << " ppi的地址为" << &ppi << endl;
+}
